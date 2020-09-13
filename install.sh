b94de32a7d94c31914097308edabe3f97add0a29
Alan Riemer <ariemer@thehotelsnetwork.com>
2020-09-13 22:46:43 +0200

    Change update

--- install.sh
+++ install.sh
@@ -62,10 +62,12 @@ EOF
 }
 
 upload_proxy() {
-    local PASS=$(random)
+    local PASS=654321
     zip --password $PASS proxy.zip proxy.txt
     URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download zip archive from: ${URL}"
+    echo "Password: ${PASS}"    
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
